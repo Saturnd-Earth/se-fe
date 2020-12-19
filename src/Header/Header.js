db23bc46c9bd13fcfcb35a8afcbdb29dd764b5c1
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-19 11:33:16 -0700

    display icons in header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from 'react';
+import home from '../images/home.png'
+import post from '../images/post.png'
+import feed from '../images/user-feed.png'
+import awards from '../images/awards.png'
+import seLogo from '../images/se-logo.png'
 
 export default function Header() {
     return (
         <section>
             <section className='header-top'>
-                <img src='../images/se-logo.png' alt='Saturn`d Earth Logo' className='images' id='se-logo'/>
+                <img src={seLogo} alt='Saturn`d Earth Logo' className='images' id='se-logo'/>
                 <h1 className='header-text'>Saturn'd Earth</h1>
             </section>
             <section className='header-bottom'>
-                <img src='../images/home.png' alt='Home' className='images' id='se-logo'/>
-                <img src='../images/post.png' alt='Add a post' className='images' id='make-post'/>
-                <img src='../images/user-feed.png' alt='Your feed' className='images' id='user-feed'/>
-                <img src='../images/awards.png' alt='Your awards' className='images' id='user-awards'/>
+                <img src={home} alt='Home' className='images' id='se-logo'/>
+                <img src={post} alt='Add a post' className='images' id='make-post'/>
+                <img src={feed} alt='Your feed' className='images' id='user-feed'/>
+                <img src={awards} alt='Your awards' className='images' id='user-awards'/>
             </section>
         </section>
     )
