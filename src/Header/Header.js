b3a0800c51a4810c2b58fcd39904fb261ed1d95f
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-19 11:13:06 -0700

    add header top

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <section>
+            <section className='header-top'>
+                <img src='../images/se-logo.png' alt='Saturn`d Earth Logo' className='images' id='se-logo'/>
+                <h1 className='header-text'>Saturn'd Earth</h1>
+            </section>
+            <div className='header-bottom'></div>
+        </section>
+    )
+}
