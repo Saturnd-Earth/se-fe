1623b140f7f4e3f1f88ddce07f0c484c8a6f996b
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-19 11:25:15 -0700

    add header icon img tags

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,7 +7,12 @@ export default function Header() {
                 <img src='../images/se-logo.png' alt='Saturn`d Earth Logo' className='images' id='se-logo'/>
                 <h1 className='header-text'>Saturn'd Earth</h1>
             </section>
-            <div className='header-bottom'></div>
+            <section className='header-bottom'>
+                <img src='../images/home.png' alt='Home' className='images' id='se-logo'/>
+                <img src='../images/post.png' alt='Add a post' className='images' id='make-post'/>
+                <img src='../images/user-feed.png' alt='Your feed' className='images' id='user-feed'/>
+                <img src='../images/awards.png' alt='Your awards' className='images' id='user-awards'/>
+            </section>
         </section>
     )
 }
