6b4c5b3cdfadda88d9f2b71fd5d421b756fed01d
Matthew Lane <themplane@gmail.com>
2020-12-19 11:47:06 -0800

    Initialize Apollo-Client by running `npm install @apollo/client graphql --save`.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+// ### This following commented out code will be used when we have an endpoint from the Backend
+
+// const client = new ApolloClient({
+//   uri: 'localhost:3000/',
+//   cache: new InMemoryCache()
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    // <ApolloProvider client={client}>
+      <App />
+    // </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
