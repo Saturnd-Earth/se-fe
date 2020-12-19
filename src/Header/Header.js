c567421d95cbadc0501d62cca72fd95f0d0440e1
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-19 12:03:48 -0700

    create grids and place header items for mobile

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Header.scss'
+
 import home from '../images/home.png'
 import post from '../images/post.png'
 import feed from '../images/user-feed.png'
@@ -9,14 +11,14 @@ export default function Header() {
     return (
         <section>
             <section className='header-top'>
-                <img src={seLogo} alt='Saturn`d Earth Logo' className='images' id='se-logo'/>
+                <img src={seLogo} alt='Saturn`d Earth Logo' id='se-logo'/>
                 <h1 className='header-text'>Saturn'd Earth</h1>
             </section>
             <section className='header-bottom'>
-                <img src={home} alt='Home' className='images' id='se-logo'/>
-                <img src={post} alt='Add a post' className='images' id='make-post'/>
-                <img src={feed} alt='Your feed' className='images' id='user-feed'/>
-                <img src={awards} alt='Your awards' className='images' id='user-awards'/>
+                <img src={home} alt='Home' className='bottom-images' id='home-icon'/>
+                <img src={post} alt='Add a post' className='bottom-images' id='make-post-icon'/>
+                <img src={feed} alt='Your feed' className='bottom-images' id='user-feed-icon'/>
+                <img src={awards} alt='Your awards' className='bottom-images' id='user-awards-icon'/>
             </section>
         </section>
     )
