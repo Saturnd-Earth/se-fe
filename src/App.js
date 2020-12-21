1bc6851a5a9aa719327a9beb070659c0d03a6134
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-20 21:46:34 -0700

    center post

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import Header from './Header/Header'
 import Post from './Post/Post.js'
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
       <Post/>
     </div>
   );
