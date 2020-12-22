5a0d7964b4063a4a54bf8a36e89c4b3e12245d51
Michael Walker <Michael.e.walker.87@gmail.com>
2020-12-22 15:35:46 -0700

    Add the routing for home my-post and make a post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 // import logo from './logo.svg';
 import Header from './Header/Header'
 import Post from './Post/Post.js'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-    </div>
+    <section>
+      <Route exact path='/' render={ () => 
+        <section className="home">
+          <Header/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+        </section>
+      }/>
+      <Route exact path='/make_post' render={ () =>
+       <section className="make-post">
+         <p>make a post page</p>
+       </section>}
+      />
+      <Route exact path='/my_post' render={ () =>
+       <section className="view-post">
+         <p>make a view page</p>
+       </section>}
+      />
+    </section>
   );
 }
 
