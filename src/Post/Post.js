47f6686a11eed9b31fe0cf5a1f9e28c68f46b524
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-22 13:08:44 -0700

    add like functionality to post div

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,10 +1,13 @@
 import React, { Component }  from 'react';
 import './Post.scss';
-import dummyIcon from '../images/dummyIcon.png'
+// icons
+import dummyIcon from '../images/dummyIcon.png';
+import defaultLike from '../images/like-white.png';
+import blueLike from '../images/like-blue.png';
 
 export default class Post extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             userInfo: {
                 userIcon: dummyIcon,
@@ -13,7 +16,12 @@ export default class Post extends Component {
             postInfo: {
                 ring: 0,
                 id: null,
-                date: null
+                date: null,
+                liked: false,
+                postContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            },
+            page: {
+                myPosts: true
             }
         }
     }
@@ -22,24 +30,35 @@ export default class Post extends Component {
         //
     }
 
+    like = () => {
+        console.log('Hello');
+        this.setState({postInfo: {
+            ...this.state.postInfo,
+            liked: !this.state.postInfo.liked
+        }})
+    }
+
     render() {
+        let likeButton;
+        !this.state.postInfo.liked ? likeButton = defaultLike : likeButton = blueLike;
         return (
             <section className='post'>
                 <section className='post-left'>
                     <div className='post-left-top'>
-                        <img src={this.state.userInfo.userIcon} alt={'User Icon'} id='user-icon'/>
+                        <img src={this.state.userInfo.userIcon} alt='User Icon' id='user-icon'/>
                     </div>
-                    <div className='post-left-bottom'></div>
+                    <div className='post-left-bottom'>
+                        <img src={likeButton} alt='Like button' id='like-button' onClick={() => this.like()}/>
+                    </div>
                 </section>
                 <section className='post-right'>
                     <div className='post-right-top'>
                             <em><strong><h5 className='post-right-top-h' id='name-header'>{this.state.userInfo.name}</h5></strong></em><br/>
-                            <em><h6 className='post-right-top-h' id='prt2'>Ring {this.state.postInfo.ring}</h6></em><br/>
+                            <em><h6 className='post-right-top-h' id='prt2'>Ring: {this.state.postInfo.ring}</h6></em><br/>
                             <em><h6 className='post-right-top-h' id='prt3'>Date: </h6></em>
                     </div>
                     <div className='post-right-bottom'>
-                        <p className='post-right-bottom-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
+                        <p className='post-right-bottom-p'>{this.state.postInfo.postContent}</p>
                     </div>
                 </section>
             </section>
