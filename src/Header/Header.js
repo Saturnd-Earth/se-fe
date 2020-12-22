9b6091e1397ed5d209c5b18d69c044a5f5b7143c
Michael Walker <Michael.e.walker.87@gmail.com>
2020-12-22 15:43:52 -0700

    Add the link to the header componet

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,6 +6,7 @@ import post from '../images/post-white.png'
 import feed from '../images/user-white.png'
 import awards from '../images/award-white.png'
 import seLogo from '../images/se-logo.png'
+import { Link, Redirect } from 'react-router-dom';
 
 export default function Header() {
     return (
@@ -15,9 +16,15 @@ export default function Header() {
                 <h1 className='header-text'>Saturn'd Earth</h1>
             </section>
             <section className='header-bottom'>
+              <Link to="/" label="home">
                 <img src={home} alt='Home' className='bottom-images' id='home-icon'/>
+              </Link>
+              <Link to="/make_post" label="my post">
                 <img src={post} alt='Add a post' className='bottom-images' id='make-post-icon'/>
-                <img src={feed} alt='Your feed' className='bottom-images' id='user-feed-icon'/>
+              </Link>
+              <Link to="/my_post" label="my post">
+                  <img src={feed} alt='Your feed' className='bottom-images' id='user-feed-icon'/>
+              </Link>
                 <img src={awards} alt='Your awards' className='bottom-images' id='user-awards-icon'/>
             </section>
         </section>
