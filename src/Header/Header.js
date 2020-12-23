3adf8addf170bfb1465f345f9ad9441652500e55
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-23 15:44:12 -0700

    Header - recenter header icons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Header.scss'
 
-import home from '../images/house-white.png'
-import post from '../images/post-white.png'
-import feed from '../images/user-white.png'
-import awards from '../images/award-white.png'
-import seLogo from '../images/se-logo.png'
+import home from '../Images/house-white.png'
+import post from '../Images/post-white.png'
+import feed from '../Images/user-white.png'
+import awards from '../Images/award-white.png'
+import seLogo from '../Images/se-logo.png'
 import { Link, Redirect } from 'react-router-dom';
 
 export default function Header() {
@@ -16,16 +16,16 @@ export default function Header() {
                 <h1 className='header-text'>Saturn'd Earth</h1>
             </section>
             <section className='header-bottom'>
-              <Link to="/" label="home">
+              <Link to="/" label="home" className='icon-links'>
                 <img src={home} alt='Home' className='bottom-images' id='home-icon'/>
               </Link>
-              <Link to="/make_post" label="my post">
+              <Link to="/make_post" label="my post" className='icon-links'>
                 <img src={post} alt='Add a post' className='bottom-images' id='make-post-icon'/>
               </Link>
-              <Link to="/my_post" label="my post">
+              <Link to="/my_post" label="my post" className='icon-links'>
                   <img src={feed} alt='Your feed' className='bottom-images' id='user-feed-icon'/>
               </Link>
-              <Link to="/awards" label="my awards">
+              <Link to="/awards" label="my awards" className='icon-links'>
                   <img src={awards} alt='Your awards' className='bottom-images' id='user-awards-icon'/>
               </Link>
             </section>
