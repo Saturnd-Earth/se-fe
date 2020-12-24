c8f343fb6d0afae7841c6659b2756d06f0766eeb
Chriscastanuela <chriscastanuela@ymail.com>
2020-12-23 18:41:26 -0700

    make header dynamic

---
+++ src/Feed/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Post from '../Post/Post.js';
+
+export default function Feed(props) {
+    return (
+        <section>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+        </section>
+    )
+}
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import './Header.scss'
-
-import home from '../Images/house-white.png'
-import post from '../Images/post-white.png'
-import feed from '../Images/user-white.png'
-import awards from '../Images/award-white.png'
+//
 import seLogo from '../Images/se-logo.png'
 import { Link, Redirect } from 'react-router-dom';
 
-export default function Header() {
-    return (
-        <section>
-            <section className='header-top'>
-                <img src={seLogo} alt='Saturn`d Earth Logo' id='se-logo'/>
-                <h1 className='header-text'>Saturn'd Earth</h1>
-            </section>
-            <section className='header-bottom'>
-              <Link to="/" label="home" className='icon-links'>
-                <img src={home} alt='Home' className='bottom-images' id='home-icon'/>
-              </Link>
-              <Link to="/make_post" label="my post" className='icon-links'>
-                <img src={post} alt='Add a post' className='bottom-images' id='make-post-icon'/>
-              </Link>
-              <Link to="/my_post" label="my post" className='icon-links'>
-                  <img src={feed} alt='Your feed' className='bottom-images' id='user-feed-icon'/>
-              </Link>
-              <Link to="/awards" label="my awards" className='icon-links'>
-                  <img src={awards} alt='Your awards' className='bottom-images' id='user-awards-icon'/>
-              </Link>
-            </section>
-        </section>
-    )
+export default function Header(props) {
+  return (
+    <section>
+      <section className='header-top'>
+        <img src={seLogo} alt='Saturn`d Earth Logo' id='se-logo'/>
+        <h1 className='header-text'>Saturn'd Earth</h1>
+      </section>
+      <section className='header-bottom'>
+        <Link to="/" label="home" className='icon-links'>
+          <img src={props.icons.home} alt='Home' className='bottom-images' id='home-icon'/>
+        </Link>
+        <Link to="/make_post" label="my post" className='icon-links'>
+          <img src={props.icons.post} alt='Add a post' className='bottom-images' id='make-post-icon'/>
+        </Link>
+        <Link to="/my_post" label="my post" className='icon-links'>
+          <img src={props.icons.feed} alt='Your feed' className='bottom-images' id='user-feed-icon'/>
+        </Link>
+        <Link to="/awards" label="my awards" className='icon-links'>
+          <img src={props.icons.awards} alt='Your awards' className='bottom-images' id='user-awards-icon'/>
+        </Link>
+      </section>
+    </section>
+  )
 }
