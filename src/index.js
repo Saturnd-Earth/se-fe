04b2b71bccf44b08b2fa7a90985d953cf9b393b3
Matthew Lane <themplane@gmail.com>
2021-01-01 21:10:04 -0800

    Add routes in the App, small file cleanup.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import App from './App';
 import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom'
-import client from './requests.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
