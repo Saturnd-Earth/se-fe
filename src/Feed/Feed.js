40fa1952e580fb978514ee3c69628264ae207c78
Chriscastanuela <chriscastanuela@ymail.com>
2021-01-04 17:36:23 -0700

    turn post divs into array and map over them

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -3,18 +3,25 @@ import './Feed.scss'
 import Post from '../Post/Post.js';
 
 export default function Feed(props) {
+    let theArray = [
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+        <Post myPostsPage={props.myPostsPage}/>,
+    ];
     return (
         <section className='feed'>
             <h1 className='header-title'>{props.headerTitle}</h1>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
-            <Post myPostsPage={props.myPostsPage}/>
+            {
+                theArray.map(i => {
+                    return i;
+                })
+            }
         </section>
     )
 }
