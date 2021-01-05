5156848eebcfc37bd772ceb67c1151c427386eaf
Chriscastanuela <chriscastanuela@ymail.com>
2021-01-04 20:19:19 -0700

    add rough lat and lon

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -53,27 +53,57 @@ export default class Post extends Component {
     render() {
         let likeButton;
         !this.state.postInfo.liked ? likeButton = defaultLike : likeButton = blueLike;
-        return (
-            <section className='post'>
-                <section className='post-left'>
-                    <div className='post-left-top'>
-                        <img src={this.state.userInfo.userIcon} alt='User Icon' id='user-icon'/>
-                    </div>
-                    <div className='post-left-bottom'>
-                        <img src={likeButton} alt='Like button' id='like-button' onClick={() => this.like()}/>
-                    </div>
+        let bottomLeft;
+        if (this.props.myPostsPage) {
+            return (
+                <section className='post'>
+                    <section className='post-left'>
+                        <div className='post-left-top'>
+                            <img src={this.state.userInfo.userIcon} alt='User Icon' id='user-icon'/>
+                        </div>
+                        <div className='post-left-bottom' style={{display: 'grid',
+                        gridTemplateRows: '1em 1em', paddingTop: '.5em'}}>
+                            <em style={{margin: 0, gridRowStart: 1, gridRowEnd: 1}}><h6  style={{margin: 0, gridRowStart: 1, gridRowEnd: 1}}>Lat: </h6></em>
+                            <em style={{margin: 0, gridRowStart: 1, gridRowEnd: 1}}><h6  style={{margin: 0, gridRowStart: 1, gridRowEnd: 1}}>-333</h6></em>
+                            <em style={{margin: 0, gridRowStart: 2, gridRowEnd: 2}}><h6  style={{margin: 0, gridRowStart: 2, gridRowEnd: 2}}>Lon: </h6></em>
+                            <em style={{margin: 0, gridRowStart: 2, gridRowEnd: 2}}><h6  style={{margin: 0, gridRowStart: 2, gridRowEnd: 2}}>-999</h6></em>
+                        </div>
+                    </section>
+                    <section className='post-right'>
+                        <div className='post-right-top'>
+                                <em><strong><h5 className='post-right-top-h' id='name-header'>{this.state.userInfo.name}</h5></strong></em><br/>
+                                <em><h6 className='post-right-top-h' id='prt2'>Ring: {this.state.postInfo.ring}</h6></em><br/>
+                                <em><h6 className='post-right-top-h' id='prt3'>Date: </h6></em>
+                        </div>
+                        <div className='post-right-bottom'>
+                            <p className='post-right-bottom-p'>{this.state.postInfo.postContent}</p>
+                        </div>
+                    </section>
                 </section>
-                <section className='post-right'>
-                    <div className='post-right-top'>
-                            <em><strong><h5 className='post-right-top-h' id='name-header'>{this.state.userInfo.name}</h5></strong></em><br/>
-                            <em><h6 className='post-right-top-h' id='prt2'>Ring: {this.state.postInfo.ring}</h6></em><br/>
-                            <em><h6 className='post-right-top-h' id='prt3'>Date: </h6></em>
-                    </div>
-                    <div className='post-right-bottom'>
-                        <p className='post-right-bottom-p'>{this.state.postInfo.postContent}</p>
-                    </div>
+            )
+        } else {
+            return (
+                <section className='post'>
+                    <section className='post-left'>
+                        <div className='post-left-top'>
+                            <img src={this.state.userInfo.userIcon} alt='User Icon' id='user-icon'/>
+                        </div>
+                        <div className='post-left-bottom'>
+                            <img src={likeButton} alt='Like button' id='like-button' onClick={() => this.like()}/>
+                        </div>
+                    </section>
+                    <section className='post-right'>
+                        <div className='post-right-top'>
+                                <em><strong><h5 className='post-right-top-h' id='name-header'>{this.state.userInfo.name}</h5></strong></em><br/>
+                                <em><h6 className='post-right-top-h' id='prt2'>Ring: {this.state.postInfo.ring}</h6></em><br/>
+                                <em><h6 className='post-right-top-h' id='prt3'>Date: </h6></em>
+                        </div>
+                        <div className='post-right-bottom'>
+                            <p className='post-right-bottom-p'>{this.state.postInfo.postContent}</p>
+                        </div>
+                    </section>
                 </section>
-            </section>
-        )
+            )
+        }
     }
 }
