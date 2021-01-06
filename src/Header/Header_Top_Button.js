6ca719307672bde5b7764af726fcd8abb9ce1f2b
Michael Walker <Michael.e.walker.87@gmail.com>
2021-01-06 15:48:40 -0700

    Fix to header logout

--- src/Header/Header_Top_Button.js
+++ src/Header/Header_Top_Button.js
@@ -9,38 +9,11 @@ import signup from '../images/sign-up-blue.png';
 export default function Header_Top_Button() {
   const [logOn, setlogOn] = useState(false);
 
-if (logOn === false) {
-  return (
-    <section className='header-top-buttons'> 
-    <NavLink to='/signup' className='header-top-buttons'>
-     <button className='logon' onClick={() => setlogOn(true)}>
-        <img 
-          src={signup} 
-          alt='sign up button' 
-          label='sign up button'
-          className='sign-up-button logon-icons'
-        />
-        Join
-      </button>
-    </NavLink>
-    <NavLink to='/login' className='header-top-buttons'>
-      <button className='logon' onClick={() => setlogOn(true)}>
-        <img 
-          src={login}
-          alt='login button' 
-          label='log in and out button'
-          className='sign-in-button logon-icons'
-        />
-        login 
-      </button>
-    </NavLink>
-    </section>
-  )
-} else {
-  return (
-    <section className='header-top-buttons' onClick={() => setlogOn(false)}> 
+  if (logOn === false) {
+    return (
+      <section className='header-top-buttons'> 
       <NavLink to='/signup' className='header-top-buttons'>
-        <button className='logon'>
+      <button className='logon' onClick={() => setlogOn(true)}>
           <img 
             src={signup} 
             alt='sign up button' 
@@ -50,17 +23,32 @@ if (logOn === false) {
           Join
         </button>
       </NavLink>
-      <button className='logon' onClick={() => setlogOn(false)}>
-        <img 
-          src={logout} 
-          alt='login button' 
-          label='log in and out button'
-          className='sign-in-button logon-icons'
-        />
-        logout 
-      </button>
-    </section>
-  )
-}
-
+      <NavLink to='/login' className='header-top-buttons'>
+        <button className='logon' onClick={() => setlogOn(true)}>
+          <img 
+            src={login}
+            alt='login button' 
+            label='log in and out button'
+            className='sign-in-button logon-icons'
+          />
+          login 
+        </button>
+      </NavLink>
+      </section>
+    )
+  } else {
+    return (
+      <section className='header-top-buttons logout-section' onClick={() => setlogOn(false)}> 
+        <button className='logon' onClick={() => setlogOn(false)}>
+          <img 
+            src={logout} 
+            alt='login button' 
+            label='log in and out button'
+            className='sign-in-button logon-icons'
+          />
+          logout 
+        </button>
+      </section>
+    )
+  }
 }
