830ca47eb6e1c3db1c1c3e159c09e32c08f94137
Matthew Lane <themplane@gmail.com>
2021-01-05 20:44:12 -0800

    Create graphQL requests

--- src/requests.js
+++ src/requests.js
@@ -1,39 +1,199 @@
-// import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-// const client = new ApolloClient({
-//   uri: 'localhost:3000/',
-//   cache: new InMemoryCache()
-// });
+export const CREATE_LIKE = gql`
+  mutation createLike($userId: Int!, $postId: Int!, $latitude: Float!, $longitude: Float!){
+    createLike(input: {
+      userId: $userId
+      postId: $postId
+      latitude: $latitude
+      longitude: $longitude
+    })
+    {
+      like{
+        id
+      }
+    }
+  }
+`;
 
-// client.query({
-//   query: gql`
-//     query getPosts($lat: lat, $long: long) {
-//       rings(lat: $lat, long: $long) {
-//         ringData {center, creationTime, hasLiked, id, lastLikedLocation, likes, size},
-//         content
-//       }
-//     }
-//   `
-// })
-// .then(result => console.log(result));
+export const CREATE_POST = gql`
+  mutation createPost($userId: Int!, $content: String!, $latitude: Float!, $longitude: Float!){
+    createLike(input: {
+      userId: $userId
+      content: $content
+      latitude: $latitude
+      longitude: $longitude
+    })
+    {
+      post{
+        id,
+        content
+        latitude
+        longitude
+        ringMinMax
+        createdAt
+        userId
+      }
+    }
+  }
+`;
 
-// client.query({
-//   query: gql`
-//     mutator createPost($lat: lat, $long: long, $content: content) {
-//       ring(lat: $lat, long: $long, content: $content)
-//     }
-//   `
-// })
-// .then(result => console.log(result));
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $password: String!){
+    createUser(input: {
+      username: $username
+      password: $password
+    })
+    {
+      user{
+        id
+        username
+      }
+    }
+  }
+`;
 
-// REQ: 'createPost'
-// variables: lat, long, content
+export const DESTROY_POST = gql`
+  mutation destroyPost($id: Int!){
+    destroyPost(input: {
+      id: $id
+    })
+    {
+      post{
+        id
+        content
+        latitude
+        longitude
+        ringMinMax
+        createdAt
+        userId
+      }
+    }
+  }
+`;
 
-// content: {
-//   text (optional)
-//   img (optional)
-//   vid (optional)
-//   url (optional)
-// }
+export const DESTROY_USER = gql`
+  mutation destroyUser($id: Int!){
+    createUser(input: {
+      id: $id
+    })
+    {
+      user{
+        id
+      }
+    }
+  }
+`;
 
-// export default client
+export const EDIT_USER = gql`
+  mutation editUser($id: Int!, $username: String!, $password: String!){
+    editUser(input: {
+      id: $id
+      username: $username
+      password: $password
+    })
+    {
+      user{
+        id
+        username
+      }
+    }
+  }
+`;
+
+export const GET_ALL_POSTS = gql`
+  query getAllPosts {
+    posts {
+      id
+      content
+      latitude
+      longitude
+      ringMinMax
+      createdAt
+      userId
+    }
+  }
+`;
+
+export const GET_POST_BY_ID = gql`
+  query getPost($id: Int!) {
+    posts (id: $id){
+      id
+      content
+      latitude
+      longitude
+      ringMinMax
+      createdAt
+      userId
+    }
+  }
+`;
+
+export const GET_USER_BY_ID = gql`
+  query getUserById($id: Int!) {
+    user(id: $id){
+      id
+      username
+    }
+  }
+`
+
+export const GET_USERS = gql`
+  query getUser {
+    users{
+      id
+      username
+    }
+  }
+`
+
+export const GET_USER_POSTS = gql`
+  query getUserPosts($userId: Int!) {
+    postsByUser(userId: $userId) {
+      id
+      content
+      latitude
+      longitude
+      ringMinMax
+      createdAt
+      userId
+    }
+  }
+`;
+
+export const GET_FEED = gql`
+  query getFeed($userId: Int!) {
+    postsByUser(userId: $userId) {
+      id
+      content
+      latitude
+      longitude
+      ringMinMax
+      createdAt
+      userId
+    }
+  }
+`;
+
+export const USER_LOGIN = gql`
+  query users($name: name, $password: password) {
+    user(name: $name, password: $password) {
+      id
+    }
+  }
+`;
+
+export const USER_SIGNUP = gql`
+  mutation userSignup($username: String!, $password: String!){
+    createUser(input: {
+      username: $username
+      password: $password
+    })
+    {
+      user{
+        id
+        username
+      }
+    }
+  }
+`;
