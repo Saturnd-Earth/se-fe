adeb7d2123a3cb3e66018c73222b0ebba546b229
Matthew Lane <themplane@gmail.com>
2021-01-08 05:33:04 -0800

    Clean up dummy data

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ import './Scss/base.scss';
 
 import React, { Component } from 'react';
 
-// Dummy Info
-import ringIcon from './images/ring-icon.png';
-import dummyIcon from './images/dummyIcon.png';
-const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-const lat = -888;
-const lon = -999;
-const name = 'John Doe';
-const dummyRing = 0
-const dummyDate = '07/04/1776'
-const dummyLastLike = 'Boston, MA'
-
 class App extends Component {
   constructor() {
     super()
@@ -31,58 +20,34 @@ class App extends Component {
   render() {
     return (
       <section className='all-pages'>
+        <Header/>
         <Route exact path='/' render={ () =>
           <section className="home">
-            <Header/>
-            <Feed
-              headerTitle='Home'
-              myPostsPage={false}
-              content={loremIpsum}
-              name={name}
-              icon={dummyIcon}
-              ring={dummyRing}
-              date={dummyDate}
-              lastLike={dummyLastLike}
-            />
+            <Feed/>
           </section>
         }/>
         <Route exact path='/make_post' render={ () =>
           <section className="make-post">
-            <Header/>
             <MakePost/>
           </section>}
         />
         <Route exact path='/my_post' render={ () =>
           <section className="view-post">
-            <Header/>
-            <Feed
-              headerTitle='My Posts'
-              myPostsPage={true}
-              content={loremIpsum}
-              lat={lat}
-              lon={lon}
-              icon={ringIcon}
-              ring={dummyRing}
-              date={dummyDate}
-              lastLike={dummyLastLike}
-            />
+            <Feed/>
           </section>}
         />
         <Route exact path='/awards' render={ () =>
           <section className="awards">
-            <Header/>
             <p>Make an awards page</p>
           </section>}
         />
         <Route exact path='/signup' render={ () =>
           <section className="signup">
-            <Header/>
             <SignUp/>
           </section>}
         />
         <Route exact path='/login' render={ () =>
           <section className="login">
-            <Header/>
             <Login/>
           </section>}
         />
