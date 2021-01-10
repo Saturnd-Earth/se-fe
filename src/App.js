5e4c6176e7b2bd6e6fb39bfefccf1f99539ff670
Michael Walker <Michael.e.walker.87@gmail.com>
2021-01-10 14:11:03 -0700

    Add sizing for the webpage for mobile tablet and big desktop

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         <Route exact path='/' render={ () =>
           <section className="home">
             <Feed
-            icon={dummyIcon}
+              icon={dummyIcon}
             />
           </section>
         }/>
