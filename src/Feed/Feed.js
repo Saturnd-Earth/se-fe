55564a4ce9403688e850832518e45c605bb8c063
Matthew Lane <themplane@gmail.com>
2021-01-10 00:33:38 -0800

    Integrate Feed with google map

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,32 +1,59 @@
-import { GET_ALL_POSTS } from '../requests.js'
-import React, { useState } from 'react';
-import '../Scss/base.scss';
+import {addRing} from '../addRing.js';
+import { GET_ALL_POSTS } from '../requests.js';
 import { gql, useQuery } from '@apollo/client';
 import { Post } from '../Post/Post.js';
+import React, { useState } from 'react';
+import '../Scss/base.scss';
 
 export default function Feed(props) {
-    let [postIndex, setPostIndex] = useState(0)
     let {loading, error, data } = useQuery(GET_ALL_POSTS);
+    let [earthMapIsLoaded, setEarthMapIsLoaded] = useState(window.earthMap !== undefined)
+    let [needNewRing, setNeedNewRing] = useState(true)
+    let [postIndex, setPostIndex] = useState(0)
+    let [ring, setRing] = useState(null)
 
-    if (loading) return <h1>LOADING POSTS...</h1>
-    if (error) return <h1>Hmm... something went wrong.</h1>
-
-    function addToIndex(num) {
+    let addToIndex = (num) => {
       let arrLength = data.posts.length
       let total = num + postIndex
       let index = ((total % arrLength) + arrLength) % arrLength
       return index
     }
 
-    let { content, createdAt, id, ringMinMax } = data.posts[postIndex]
+    if (loading) return <h1>LOADING POSTS...</h1>
+    if (error) return <h1>Hmm... something went wrong.</h1>
+
+    let { content, createdAt, id, latitude, longitude, ringMinMax } = data.posts[postIndex]
     let [min, max] = ringMinMax.slice(1, -1).split(', ').map( char => +char )
+
+    let lat = () => latitude
+    let lng = () => longitude
+    let newRing = addRing({lat, lng}, min, max)
+
+    if (needNewRing && earthMapIsLoaded) {
+      if (ring) {
+        ring.setMap( null )
+      }
+      setRing(newRing)
+      setNeedNewRing(false)
+
+    } else {
+      window.setTimeout( () => setEarthMapIsLoaded(window.earthMap !== undefined), 100)
+    }
 
     return (
         <section className='feed'>
             <h1 className='header-title'>{props.headerTitle}</h1>
             <Post content={content} createdAt={createdAt} id={id} ring={[min, max]}/>
-            <button onClick={ () => setPostIndex( addToIndex( -1 )) }>Previous</button>
-            <button onClick={ () => setPostIndex( addToIndex( 1 )) }>Next</button>
+
+            <button
+              onClick={ () => {
+                setNeedNewRing(true)
+                setPostIndex( addToIndex( -1 ))
+              }}
+            >Previous</button>
+
+            <button
+              onClick={ () => setPostIndex( addToIndex( 1 )) }>Next</button>
         </section>
     )
 }
