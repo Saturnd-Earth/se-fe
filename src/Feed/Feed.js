4a51b6ef5f9f03c4ac98e83e2dc3d0bee5136df1
Matthew Lane <themplane@gmail.com>
2021-01-13 09:54:52 -0800

    cleanup and refactoring

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -10,24 +10,29 @@ import prev from '../images/prev-mixed.png'
 import next from '../images/next-mixed.png'
 
 export default function Feed(props) {
+  console.log(props)
     let {loading, error, data } = useQuery(GET_FEED, {
       variables: {
         latitude: props.position.lat || 0,
         longitude: props.position.lng || 0
       }
     });
-    let [earthMapIsLoaded, setEarthMapIsLoaded] = useState(window.earthMap !== undefined)
     let [needNewRing, setNeedNewRing] = useState(true)
     let [postIndex, setPostIndex] = useState(0)
 
     if (loading) return <Loading/>
     if (error) return <h1>Hmm... something went wrong.</h1>
-
-    let { content, createdAt, id, likes, latitude, longitude, postType, ringMinMax, url, text } = data.postsUserCanIncrease[postIndex]
+    if (!data || !data.postsUserCanIncrease || data.postsUserCanIncrease.length === 0) {
+      return (
+        <h1>There are no posts for your area. Be the first!</h1>
+      )
+    }
+    console.log(data)
+    let { createdAt, id, likes, latitude, longitude, postType, ringMinMax, url, text } = data.postsUserCanIncrease[postIndex]
     let [min, max] = ringMinMax.slice(1, -1).split(', ').map( char => +char )
     let center = {lat: () => latitude, lng: () => longitude}
 
-    if (needNewRing && earthMapIsLoaded) {
+    if (needNewRing && window.earthMapIsInitialized) {
       removeAllRings()
       window.earthMap.setZoom(10)
       window.earthMap.setCenter({lat: latitude, lng: longitude})
@@ -36,14 +41,13 @@ export default function Feed(props) {
 
       removeAllLikes()
       likes.forEach( like => addLike( {lat: latitude, lng: longitude} ))
-
-    } else if (!earthMapIsLoaded) {
-      window.setTimeout( () => setEarthMapIsLoaded(window.earthMap !== undefined), 100)
     }
 
     return (
         <section className='feed'>
-            <h1 className='header-title'>{props.headerTitle}</h1>
+            <h1 className='header-title'>
+              {props.userData.id === null ? 'Please login to like a post' : ''}
+            </h1>
             <Post
               center={center}
               content={text}
